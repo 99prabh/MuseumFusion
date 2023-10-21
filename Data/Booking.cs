9c3d572332c2fb133e5ef515949e9bf45271f185
99prabh <133125732+99prabh@users.noreply.github.com>
2023-10-20 23:24:07 -0400

    Added the Booking class to the project, which represents a booking for an event at the museum. The class includes two public properties: BookedEvent (for the event name) and People (a List of Person objects for attendees). The People list is initialized in the constructor to avoid null reference exceptions. I've also included a ToString method to provide a formatted string representation of a booking, listing the event name and the names of attending people.

---
+++ Data/Booking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace MuseumFusion.Data;
+
+/// <summary>
+/// Represents a booking for an event at the museum.
+/// </summary>
+public class Booking
+{
+    /// <summary>
+    /// The name of the booked event.
+    /// </summary>
+    public string BookedEvent { get; set; }
+
+    /// <summary>
+    /// The list of people attending the event.
+    /// </summary>
+    public List<Person> People { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the Booking class.
+    /// </summary>
+    public Booking()
+    {
+        People = new List<Person>();  // Initialize the list to avoid null reference exceptions.
+    }
+
+    /// <summary>
+    /// Returns a string representation of the Booking object.
+    /// </summary>
+    /// <returns>A string containing the booked event name and the list of attending people.</returns>
+    public override string ToString()
+    {
+        return $"Booked Event: {BookedEvent}\nAttending People: {string.Join(", ", People)}";
+    }
+}
