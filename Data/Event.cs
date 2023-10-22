d42ae150a6a6791e7730332d7107cbb0d2cea898
Babita <a00280963@mycambrian.ca>
2023-10-22 18:17:32 -0400

    add museeum model

--- Data/Event.cs
+++ Data/Event.cs
@@ -1,8 +1,34 @@
 ﻿using System;
 
-namespace MuseumFusion.Data;
-
-public class Event
+namespace MuseumFusion.Data
 {
+    public class Event
+    {
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public string museum { get; set; }
+
+        public DateTime startTime { get; set; }
+
+        public string description { get; set; }
+
+        public int price { get; set; }
+
+        public string imageURL { get; set; }
+
+        public Event()
+        {
+            this.id = null;
+            this.name = null;
+            this.museum = null;
+            this.startTime = default(DateTime);
+            this.description = null;
+            this.price = 0;
+            this.imageURL = null;
+        }
+    }
 }
+
 
---
+++ Data/Museuem.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MuseumFusion.Data
+{
+	public class Museum
+	{
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public string hours { get; set; }
+
+        public List<string> events { get; set; }
+
+        public string imageURL { get; set; }
+    }
+}
+
