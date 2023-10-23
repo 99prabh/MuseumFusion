35540a9ffcc21a60123d7ed15e2572782bf902a2
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-10-22 22:58:55 -0400

    Added User class.

---
+++ Data/User.cs
@@ -0,0 +1,20 @@
+using System;
+namespace MuseumFusion.Data
+{
+    public class User
+    {
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public DateTime birthDate { get; set; }
+
+        public string password { get; set; }
+
+        public List<string> followedMuseums { get; set; } = new List<string>();
+
+        public Boolean isAdmin { get; set; } = false;
+
+        public List<Booking> eventBookings { get; set; } = new List<Booking>();
+    }
+}
