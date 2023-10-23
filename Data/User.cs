03da1866a209c7b40d29514b7b65b023ec981af7
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-10-22 23:02:16 -0400

    The class represents a user in the museum management system, featuring properties such as id, name, birthDate, password, followedMuseums, isAdmin, and eventBookings. Each property is carefully defined to store specific user-related information, ensuring a well-structured user profile. Default values are set for properties like followedMuseums and eventBookings, enhancing usability and preventing null references.

--- Data/User.cs
+++ Data/User.cs
@@ -1,20 +1,46 @@
 using System;
+
 namespace MuseumFusion.Data
 {
+    /// <summary>
+    /// The User class represents a user in the museum management system. It contains user-specific information 
+    /// such as ID, name, birthdate, password, followed museums, admin status, and event bookings.
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Gets or sets the unique ID of the user.
+        /// </summary>
         public string id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
         public string name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the birthdate of the user.
+        /// </summary>
         public DateTime birthDate { get; set; }
 
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
         public string password { get; set; }
 
+        /// <summary>
+        /// Gets or sets the list of museums followed by the user.
+        /// </summary>
         public List<string> followedMuseums { get; set; } = new List<string>();
 
-        public Boolean isAdmin { get; set; } = false;
+        /// <summary>
+        /// Gets or sets a boolean indicating whether the user is an admin (true) or not (false).
+        /// </summary>
+        public bool isAdmin { get; set; } = false;
 
+        /// <summary>
+        /// Gets or sets the list of bookings made by the user for various events.
+        /// </summary>
         public List<Booking> eventBookings { get; set; } = new List<Booking>();
     }
 }
